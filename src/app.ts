13ccd387bbc03cde9d4586153ab8a98a1cd577bf
paomrtz <estefymartinezc235y@gmail.com>
2024-09-13 01:45:46 -0600

    Actualización de dependencias y configuración: conexion DB y variables de entorno

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,22 @@
 import express, { request, Request, Response } from "express";
+import { envs } from "./config/env";
+import { MongoDatabase } from "./data/init";
+
+console.log(envs.PORT);
 
 const app = express();
+
+(async () => {
+    await MongoDatabase.connect({
+        mongoUrl: envs.MONGO_URL,
+        dbName: envs.MONDO_DB
+    });
+})();
 
 app.get('/',(req:Request, res:Response)=>{
     res.send("Hola")
 });
 
-app.listen(3000,()=>{
-    console.log("Server running on PORT 3000")
+app.listen(envs.PORT, () => {
+    console.log(`Server running on PORT ${envs.PORT}`);
 });
