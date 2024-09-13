d31a46ef5e9ddb640b4b4da26746f359fd1e8a8f
paomrtz <estefymartinezc235y@gmail.com>
2024-09-13 02:31:53 -0600

    Implementación del esquema y verificación de registro POST

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,41 @@
-import express, { request, Request, Response } from "express";
+import express, { Request, Response } from "express";
 import { envs } from "./config/env";
 import { MongoDatabase } from "./data/init";
+import { MonoMapModel } from "./data/models/init.models";
 
 console.log(envs.PORT);
 
 const app = express();
+app.use(express.json());
 
 (async () => {
-    await MongoDatabase.connect({
-        mongoUrl: envs.MONGO_URL,
-        dbName: envs.MONDO_DB
-    });
+    try {
+        await MongoDatabase.connect({
+            mongoUrl: envs.MONGO_URL,
+            dbName: envs.MONDO_DB
+        });
+        console.log("Database connected successfully");
+    } catch (error) {
+        console.error("Database connection failed", error);
+    }
 })();
 
-app.get('/',(req:Request, res:Response)=>{
-    res.send("Hola")
+app.get('/', (req: Request, res: Response) => {
+    res.send("Hola");
+});
+
+app.post("/", async (req: Request, res: Response) => {
+    try {
+        const { lat, lng, genre, age } = req.body;
+        const newMono = await MonoMapModel.create({
+            lat,
+            lng,
+            genre,
+            age
+        });
+        return res.json(newMono);
+    } catch (error) {
+    }
 });
 
 app.listen(envs.PORT, () => {
